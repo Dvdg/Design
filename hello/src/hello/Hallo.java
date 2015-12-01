d030bc8e71b84c96e23bd3a5c463ecea371c5f1a
David <David.van.de.giessen@live.nl>
2015-12-01 13:08:54 +0100

    k

---
+++ hello/src/hello/Hallo.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hallo {
+	
+	public static void main(String[] args){
+		System.out.println("Hallo wereld1!");
+	}
+	
+	public int min(int x, int y){
+		return x - y;
+	}
+
+}
