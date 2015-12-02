c83db84dfc9b29812ba5b364d9903d77e958ae14
David <David.van.de.giessen@live.nl>
2015-12-02 09:25:32 +0100

    Hello World

--- hello/src/hello/Hallo.java
+++ hello/src/hello/Hallo.java
@@ -4,7 +4,7 @@ public class Hallo {
 	
 
 	public static void main(String[] args){
-		System.out.println("DAVID IS EEN NSGAYER");
+		System.out.println("Hello World!");
 	}
 	
 	public int som(int x, int y){
