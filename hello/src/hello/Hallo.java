daae36c3c524fdbdb1457a9e17a617cb249a6d1d
David <David.van.de.giessen@live.nl>
2015-12-02 09:31:06 +0100

    j

--- hello/src/hello/Hallo.java
+++ hello/src/hello/Hallo.java
@@ -4,7 +4,7 @@ public class Hallo {
 	
 
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		System.out.println("Hello World1!");
 	}
 	
 	public int som(int x, int y){
