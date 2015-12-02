8782a6166a075dac45649298dab7a053bd4b1605
Thijs <Thijs@Dell-Thijs>
2015-12-02 09:06:36 +0100

    sDA

--- hello/src/hello/Hallo.java
+++ hello/src/hello/Hallo.java
@@ -3,7 +3,7 @@ package hello;
 public class Hallo {
 	
 	public static void main(String[] args){
-		System.out.println("Hallo wereld1 thijs breekt in :D !");
+		System.out.println("Doei!");
 	}
 	
 	public int min(int x, int y){
